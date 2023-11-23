b0ac81154b76ab964811b0b4d1681a5f2b14801a
Hsitirb <hsitirb1@gmail.com>
2023-11-23 21:21:41 +0000

    Day 1 Puzzle 1: Add puzzle input textfile and code to run on CLI script execution

--- Day1/elf.py
+++ Day1/elf.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass, field
 
+
 @dataclass
 class Elf:
     food_list: list[int] = field(default_factory=list)
 
     def get_calories(self):
         return sum(self.food_list)
-    
+
+
 def elf_with_max_calories(calories_input):
     elves = []
     current_elf = Elf()
@@ -18,3 +20,9 @@ def elf_with_max_calories(calories_input):
             current_elf.food_list.append(int(line))
     calories_list = [elf.get_calories() for elf in elves]
     return max(calories_list)
+
+
+if __name__ == "__main__":
+    with open("puzzle_input.txt") as f:
+        calories_input = f.read()
+    print(elf_with_max_calories(calories_input))
