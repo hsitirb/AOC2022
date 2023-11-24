9e997cb2ab0730309e9ed095fc94c48b761ce973
Hsitirb <hsitirb1@gmail.com>
2023-11-24 13:49:13 +0000

    Day 1 Problem 2: Add test

--- Day1/test_elf.py
+++ Day1/test_elf.py
@@ -1,5 +1,5 @@
 import pytest
-from elf import elf_with_max_calories
+from elf import elf_with_max_calories, top3_elves_with_max_calories
 
 sample_input = """1000
 2000
@@ -15,9 +15,15 @@ sample_input = """1000
 9000
 
 10000"""
-expected = 24000
+expected1 = 24000
+expected2 = 45000
 
-@pytest.mark.parametrize("input, expected", [[sample_input, expected]])
-def test_elf(input, expected):
+
+@pytest.mark.parametrize("input, expected", [[sample_input, expected1]])
+def test_elf_part1(input, expected):
     assert elf_with_max_calories(input) == expected
 
+
+@pytest.mark.parametrize("input, expected", [[sample_input, expected2]])
+def test_elf_part2(input, expected):
+    assert top3_elves_with_max_calories(input) == expected
